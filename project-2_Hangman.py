ed043316a3e61ab55e9e8b7eefa4556e3b93399e
Manoj-Kumar-03 <lingala.manoj3@gmail.com>
2026-06-30 11:44:07 +0530

    Enhance game flow and user experience
    Refactor Hangman game logic and improve input validation.

--- project-2_Hangman.py
+++ project-2_Hangman.py
@@ -1,4 +1,5 @@
-import  random
+import random
+
 logo = r'''
  _                                             
 | |                                            
@@ -8,7 +9,8 @@ logo = r'''
 |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                     __/ |                      
                    |___/                       
- '''
+'''
+
 stages = [
 r'''
   +---+
@@ -65,52 +67,74 @@ r'''
       |
       |
       |
-=========''']
+========='''
+]
 
-word_list = ["python", "computer", "keyboard", "monitor", "program", "network", "internet", "science", "diamond", "elephant", "giraffe", "mountain", "adventure", "treasure", "airplane", "hospital", "umbrella", "sandwich", "chocolate", "pineapple", "backpack", "football", "calendar", "notebook", "rainbow"]
+word_list = [
+    "python", "computer", "keyboard", "monitor", "program",
+    "network", "internet", "science", "diamond", "elephant",
+    "giraffe", "mountain", "adventure", "treasure", "airplane",
+    "hospital", "umbrella", "sandwich", "chocolate", "pineapple",
+    "backpack", "football", "calendar", "notebook", "rainbow"
+]
 
 lives = 6
 
 print(logo)
-random_word = random.choice(word_list).lower()
-placeholder = ''
-word_len = len(random_word)
-for _ in range(word_len):
-    placeholder += '_'
 
-print(random_word)
-print("Word to guess : ", placeholder)
+random_word = random.choice(word_list)
+
+correct_letters = []
+guessed_letters = []
 
 game_over = False
-correct_letters = []
 
 while not game_over:
-    print(f"You have {lives}/6 lives left")
-    guess = input("Guess a letter: ").lower()
-    if guess in correct_letters:
-        print(f"You have already guessed {guess}")
-    display = ''
-    for letter in random_word:
-        if letter == guess:
-            display += letter
-            correct_letters.append(letter)
-        elif letter in correct_letters:
-            display += letter
-        else:
-            display += '_'
 
-    print("Word to guess: " + display)
-
-    if guess not in random_word:
-        lives -= 1
-        print(f"You guessed {guess}, that is not in word.you lose a life")
-        if lives == 0:
-            game_over = True
-            print(f'You Lose!, the chosen word is {random_word}')
-
-    if '_' not in display:
-        game_over = True
-        print('You Won!!')
-
+    print("\n" + "=" * 40)
+    print(f"Lives Remaining: {lives}/6")
     print(stages[lives])
 
+    display = ""
+    for letter in random_word:
+        if letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word:", " ".join(display))
+
+    if guessed_letters:
+        print("Guessed Letters:", " ".join(sorted(guessed_letters)))
+
+    guess = input("\nGuess a letter: ").lower()
+
+    # Input validation
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter only one alphabet letter.")
+        continue
+
+    # Already guessed
+    if guess in guessed_letters:
+        print(f"You already guessed '{guess}'. Try another letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in random_word:
+        correct_letters.append(guess)
+        print(f"Good job! '{guess}' is in the word.")
+    else:
+        lives -= 1
+        print(f"'{guess}' is not in the word. You lose a life.")
+
+    # Win condition
+    if all(letter in correct_letters for letter in random_word):
+        print("\n Congratulations! You guessed the word:", random_word)
+        game_over = True
+
+    # Lose condition
+    elif lives == 0:
+        print(stages[lives])
+        print(f"\n💀 Game Over! The word was '{random_word}'.")
+        game_over = True
